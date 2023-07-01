50f6b2245b6dd4302d0434e617657ae13a474b1b
zahar <astolfotraper@gmail.com>
2023-07-01 07:29:47 +0300

    Дз

---
+++ a1.py
@@ -0,0 +1,39 @@
+# ---------------------------------------------------------------------------------------------------------- #
+
+first_summa = 550966
+first_procent = 8105 + 8286 + 8200 + 7850 + 8019 + 7673 + 7817 + 7718 + 7131 + 7519 + 7182 + 7318
+first_osnovnoy_dolg = 5870 + 5689 + 5775 + 6125 + 5956 + 6302 + 6158 + 6258 + 6844 + 6456 + 6793 + 6657
+first_pereplata = 50966
+first_ostatok = first_summa - first_osnovnoy_dolg
+
+first_overpay = first_pereplata + first_procent + first_ostatok
+print(first_overpay)
+
+# ---------------------------------------------------------------------------------------------------------- #
+
+second_summa = 531999
+second_procent = (9575 + 9813 + 9735 + 9345 + 9570 + 9182 + 9380 + 9288 + 8608 + 9102 + 8721 + 8914) * 0.96
+second_osnovnoy_dolg = 4400 + 4162 + 4240 + 4630 + 4405 + 4793 + 4595 + 4687 + 5367 + 4873 + 5254 + 5061    
+second_pereplata = 31999
+second_ostatok = second_summa - second_osnovnoy_dolg
+
+second_overpay = second_pereplata + second_procent + second_ostatok
+print(second_overpay)
+
+# ---------------------------------------------------------------------------------------------------------- #
+
+third_summa = 604544
+third_procent = (7900 + 8081 + 8002 + 7665 + 7836 + 7503 + 7650 + 7559 + 6991 + 7379 + 7055 + 7197) * 0.95
+third_osnovnoy_dolg = 6075 + 5894 + 5973 + 6310 + 6139 + 6472 + 6325 + 6416 + 6984 + 6596 + 6920 + 6778
+third_pereplata = 104544
+third_ostatok = third_summa - third_osnovnoy_dolg
+
+third_overpay = third_pereplata + third_procent + third_ostatok
+print(third_overpay)
+
+# ---------------------------------------------------------------------------------------------------------- #
+
+
+
+
+
